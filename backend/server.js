178864e48d2f69dcf82bd408fb75d6c3ae5bff88
Arshad-97 <agmarshad97@gmail.com>
2020-10-11 17:10:00 +0530

    Step-2 database connection estabalised

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require ('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dontenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use((cors()));
+app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection estabalised succefully");
+})
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
