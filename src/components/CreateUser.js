fa0bbd9c46dea1c3676a60170b25c9456702d67d
Arshad-97 <agmarshad97@gmail.com>
2020-10-20 11:50:41 +0530

    Step-10 : Created create User component design

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
 
 export default class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+
+        console.log(user);
+        window.location = '/';
+    }
+
     render() {
         return (
             <div>
-                <p>excecise list</p>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>User Name:</label>
+                        <input type="text"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}></input>
+                    </div>
+                    <div>
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
